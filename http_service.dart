c89b39f718d0c8519ba67e423b18f2d7f7364cc7
nabihahhh <35028821+nabihahhh@users.noreply.github.com>
2021-06-30 02:44:29 +0800

    Add files via upload

---
+++ http_service.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'post_model.dart';
+
+class HttpService {
+  final String postsURL =
+      "https://my-json-server.typicode.com/nabihahhh/demo/posts";
+
+  Future<List<Post>> getPosts() async {
+    Response res = await get(Uri.parse(postsURL));
+
+    if (res.statusCode == 200) {
+      List<dynamic> body = jsonDecode(res.body);
+
+      List<Post> posts = body
+          .map(
+            (dynamic item) => Post.fromJson(item),
+          )
+          .toList();
+
+      return posts;
+    } else {
+      throw "Unable to retrieve posts.";
+    }
+  }
+}
