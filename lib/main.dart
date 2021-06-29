347ca650278e48861d9d7486e947be2dced0290b
Wan Nurul Adibah <55779668+deebtarmizi104@users.noreply.github.com>
2021-06-30 03:21:53 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:group_ass/LoginSetup/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Demo',
+      theme: ThemeData(
+        primaryColor: Colors.purple[400],
+      ),
+      home: WelcomePage(),
+    );
+  }
+}
